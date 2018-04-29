6fc4f2a48cc08d3904d773607ce2c1d4db79a6f6
mrefs <mrefsnider@gmail.com>
2018-04-29 14:20:51 -0400

    Initial commit Changes to be committed: new file:   .DS_Store new file:   Assets/.DS_Store new file:   Assets/Binder scans/File_001.png new file:   Assets/Binder scans/File_002.png new file:   Assets/Binder scans/File_003.png new file:   Assets/Binder scans/File_004.png new file:   Assets/Binder scans/File_005.png new file:   Assets/Binder scans/File_006.png new file:   Assets/Binder scans/File_007.png new file:   Assets/Binder scans/File_008.png new file:   "Assets/Binder scans/Icon\r" new file:   Assets/HowToCommentCodeInOracle021215.pdf new file:   "Assets/Icon\r" new file:   ISM 4212 Presentation.gslides new file:   "Icon\r" new file:   Instance Charts Note.docx new file:   Mod 3 Deliverables/FuentesRefsniderFile1.sql new file:   Mod 3 Deliverables/FuentesRefsniderFile2.sql new file:   Mod 3 Deliverables/FuentesRefsniderFile3.sql new file:   "Mod 3 Deliverables/Icon\r" new file:   Mod 3 Deliverables/Instance_Charts.pdf new file:   Mod1Revised/.DS_Store new file:   Mod1Revised/00-Cycle Ivans Logo-outside cover.jpg new file:   Mod1Revised/01-Cycle Ivans-inside cover.png new file:   Mod1Revised/01-mod2coversheet.pdf new file:   Mod1Revised/02-mod2gr4212Spring2018.pdf new file:   Mod1Revised/03-Narrative.pdf new file:   Mod1Revised/04-Cycle Ivans Invoice.pdf new file:   Mod1Revised/05-Mod-1-Business-Rules.pdf new file:   Mod1Revised/06-data-normalization-raw-0nf.pdf new file:   Mod1Revised/07-data-normalization-0nf-1nf.pdf new file:   Mod1Revised/08-data-normalization-2nf-3nf.pdf new file:   Mod1Revised/09-data-normalization-3nf-ds.pdf new file:   Mod1Revised/10-ERD-CYCLE-IVANS.pdf new file:   Mod1Revised/11-Relationship sentences.pdf new file:   Mod1Revised/12-ISM4212projMod2InstSpring2018.pdf new file:   Mod2Deliverables/.DS_Store new file:   "Mod2Deliverables/Icon\r" new file:   Mod2Deliverables/bill-tos-ic.pdf new file:   Mod2Deliverables/invoices-ic.pdf new file:   Mod2Deliverables/items-ic.pdf new file:   Mod2Deliverables/items-sold-ic.pdf new file:   Mod2Deliverables/owners-ic.pdf new file:   Mod2Deliverables/spool.pdf new file:   Mod2Deliverables/vehicles-ic.pdf new file:   "Mod2Revised/Icon\r" new file:   Working Files/.DS_Store new file:   Working Files/05-Narrative.gdoc new file:   Working Files/07-Mod-1-Business-Rules.gdoc new file:   Working Files/13-Relationship sentences.gdoc new file:   Working Files/1DDL_drop_statments.sql new file:   Working Files/2DDL_create_statements.sql new file:   Working Files/3DDL_Sequence_Statements.sql new file:   Working Files/4DDL_Insert_Statements.sql new file:   Working Files/5DDL_Query_Statements.sql new file:   Working Files/Carter.m4a new file:   Working Files/Data_Normalization.xlsx new file:   Working Files/ERD_CYCLE_IVANS.vsd new file:   "Working Files/Icon\r" new file:   Working Files/Instance_Charts.xlsx new file:   Working Files/Instance_Charts.xlsx.gsheet new file:   Working Files/Mod2_Spool.LST new file:   Working Files/New SQL/.DS_Store new file:   "Working Files/New SQL/Icon\r" new file:   Working Files/New SQL/New Inserts WS - ALL SQL.txt new file:   Working Files/New SQL/New Inserts WS - ALL SQL.txt.gdoc new file:   Working Files/bDB.sql new file:   Working Files/mod-2-biz-rules.gdoc new file:   Working Files/scratch.sql new file:   Working Files/scratch.txt

---
+++ Mod 3 Deliverables/FuentesRefsniderFile1.sql
@@ -0,0 +1,1208 @@
+-- JAY FUENTES AND MATT REFSNIDER
+
+-- START CREATE TABLE STATEMENTS
+
+-- CREATE TABLES
+
+CREATE TABLE vehicles_ci
+(
+   vehicle_vin  VARCHAR2(17)
+      CONSTRAINT vcle_vehicle_vin_pk PRIMARY KEY,
+   vehicle_make  VARCHAR2(20)
+      CONSTRAINT vcle_vehicle_make_nn NOT NULL,
+   vehicle_model VARCHAR2(20)
+      CONSTRAINT vcle_vehicle_model_nn NOT NULL,
+   vehicle_year  NUMBER(4)
+      CONSTRAINT vcle_vehicle_year_nn NOT NULL,
+   vehicle_color VARCHAR2(20)
+      CONSTRAINT vcle_vehicle_color_nn NOT NULL,
+   vehicle_dealer_no CHAR(5)
+);
+
+CREATE TABLE bill_tos_ci
+(
+   bill_to_no   NUMBER(5)
+      CONSTRAINT bill_bill_to_no_pk PRIMARY KEY,
+   bill_to_first_name VARCHAR2(20)
+      CONSTRAINT bill_bill_to_first_name_nn NOT NULL,
+   bill_to_last_name VARCHAR2(20)
+      CONSTRAINT bill_bill_to_last_name_nn NOT NULL,
+   bill_to_street_1  VARCHAR2(30)
+      CONSTRAINT bill_bill_to_street_1_nn NOT NULL,
+   bill_to_street_2  VARCHAR2(15),
+   bill_to_city   VARCHAR2(30)
+      CONSTRAINT bill_bill_to_city_nn NOT NULL,
+   bill_to_state CHAR(2)
+      CONSTRAINT bill_bill_to_state_nn NOT NULL,
+   bill_to_zip   CHAR(5)
+      CONSTRAINT bill_bill_to_zip_nn NOT NULL
+);
+
+CREATE TABLE items_ci
+(
+   item_no VARCHAR2(10)
+      CONSTRAINT item_item_no_pk PRIMARY KEY,
+   item_name VARCHAR2(20)
+      CONSTRAINT item_item_name_nn NOT NULL,
+   item_description VARCHAR2(30)
+      CONSTRAINT item_item_description_nn NOT NULL,
+   item_unit_price  NUMBER(6,2)
+      CONSTRAINT item_item_unit_price_nn NOT NULL
+);
+
+CREATE TABLE owners_ci
+(
+   vehicle_vin  VARCHAR2(17),
+   bill_to_no  NUMBER(5),
+      CONSTRAINT owners_vcle_vin_bill_to_no_pk
+         PRIMARY KEY (vehicle_vin,bill_to_no),
+      CONSTRAINT owners_vehicle_vin_fk
+         FOREIGN KEY (vehicle_vin)
+         REFERENCES vehicles_ci(vehicle_vin),
+      CONSTRAINT owners_bill_to_no_fk
+         FOREIGN KEY (bill_to_no)
+         REFERENCES bill_tos_ci(bill_to_no)
+);
+
+CREATE TABLE invoices_ci
+(
+   invoice_no  NUMBER(5)
+      CONSTRAINT invs_invoice_no_pk PRIMARY KEY,
+   invoice_date DATE
+      CONSTRAINT invs_inv_date_nn NOT NULL,
+   invoice_odometer  NUMBER(6)
+      CONSTRAINT invs_inv_odometer_nn NOT NULL,
+   invoice_tax  NUMBER(4,2)
+      CONSTRAINT invs_inv_tax_nn NOT NULL,
+   invoice_payment_cred  NUMBER(5,2),
+   bill_to_no  NUMBER(5)
+      CONSTRAINT invs_bill_to_no_nn NOT NULL,
+   vehicle_vin  VARCHAR2(17)
+      CONSTRAINT invs_vcle_vehicle_vin_nn NOT NULL,
+      CONSTRAINT invs_bill_to_no_vin_fk
+         FOREIGN KEY (bill_to_no,vehicle_vin)
+         REFERENCES owners_ci(bill_to_no,vehicle_vin)
+   );
+
+CREATE TABLE items_sold_ci
+(
+   invoice_no  NUMBER(5),
+   item_no  VARCHAR2(10),
+   item_qty_sold  NUMBER(2)
+      CONSTRAINT items_sold_item_qty_sold_nn  NOT NULL,
+   item_unit_price NUMBER(6,2)
+      CONSTRAINT items_sold_item_unit_price_nn
+         NOT NULL,
+   CONSTRAINT items_sold_inv_no_item_no_pk
+      PRIMARY KEY (invoice_no,item_no),
+   CONSTRAINT items_sold_invoice_no_fk
+      FOREIGN KEY (invoice_no)
+      REFERENCES invoices_ci(invoice_no),
+   CONSTRAINT prods_item_no_fk
+      FOREIGN KEY (item_no)
+      REFERENCES items_ci(item_no)
+);
+
+-- TO GET INFORMATION ABOUT TABLES
+
+DESCRIBE vehicles_ci
+DESCRIBE bill_tos_ci
+DESCRIBE items_ci
+DESCRIBE owners_ci
+DESCRIBE invoices_ci
+DESCRIBE items_sold_ci
+
+-- TO GET INFORMATION ABOUT CONSTRAINTS
+
+COLUMN constraint_name FORMAT A32;
+COLUMN column_name FORMAT A20;
+COLUMN table_name FORMAT A15;
+SELECT constraint_name, column_name,table_name
+FROM user_cons_columns
+WHERE constraint_name NOT LIKE 'BIN%'
+ORDER BY constraint_name;
+
+-- END CREATE TABLE STATEMENTS
+
+-- START CREATE SEQUENCE STATEMENTS
+
+-- CREATE SEQUENCES
+
+CREATE SEQUENCE bill_tos_bill_to_no_seq
+	START WITH 8923
+	INCREMENT BY 1
+	MAXVALUE 99999
+	NOCYCLE
+	NOCACHE;
+
+CREATE SEQUENCE items_item_no_seq
+	START WITH 1122334455
+	INCREMENT BY 1
+	MAXVALUE 9999999999
+	NOCYCLE
+	NOCACHE;
+
+CREATE SEQUENCE invoices_invoice_no_seq
+	START WITH 25333
+	INCREMENT BY 1
+	MAXVALUE 99999
+	NOCYCLE
+	NOCACHE;
+
+-- TO GET INFORMATION ABOUT SEQUENCES
+
+SELECT sequence_name, increment_by
+FROM user_sequences;
+
+-- END CREATE SEQUENCE STATEMENTS
+
+-- START INSERT STATEMENTS
+
+-- INSERT DATA TO VEHICLES_CI TABLE
+
+-- INSERT DATA TO VEHICLES_CI TABLE
+
+INSERT INTO vehicles_ci
+(
+	vehicle_vin,
+	vehicle_make,
+	vehicle_model,
+	vehicle_year,
+	vehicle_color,
+	vehicle_dealer_no
+)
+VALUES
+(
+	'121V5G1H221457H47',
+	'HONDA',
+	'SHADOW1100',
+	1985,
+	'BLACK',
+	32582
+);
+
+INSERT INTO vehicles_ci
+(
+	vehicle_vin,
+	vehicle_make,
+	vehicle_model,
+	vehicle_year,
+	vehicle_color,
+	vehicle_dealer_no
+)
+VALUES
+(
+	'4T4BF3EK6BR115086',
+	'HARLEY',
+	'RAVEN2000',
+	2018,
+	'LASER BLUE',
+	45447
+);
+
+INSERT INTO vehicles_ci
+(
+	vehicle_vin,
+	vehicle_make,
+	vehicle_model,
+	vehicle_year,
+	vehicle_color,
+	vehicle_dealer_no
+)
+VALUES
+(
+	'2GBHG31MXD4127143',
+	'SUZUKI',
+	'NINJA1000',
+	2001,
+	'WHITE',
+	12258
+);
+
+INSERT INTO vehicles_ci
+(
+	vehicle_vin,
+	vehicle_make,
+	vehicle_model,
+	vehicle_year,
+	vehicle_color,
+	vehicle_dealer_no
+)
+VALUES
+(
+	'1FUBCYCS35HN42268',
+	'KAWASAKI',
+	'ROADSTER788',
+	2016,
+	'BLACK',
+	58445
+);
+
+INSERT INTO vehicles_ci
+(
+	vehicle_vin,
+	vehicle_make,
+	vehicle_model,
+	vehicle_year,
+	vehicle_color,
+	vehicle_dealer_no
+)
+VALUES
+(
+	'1C4GJWAG7CL176718',
+	'HONDA',
+	'STREAK1',
+	2009,
+	'BLUE',
+	NULL
+);
+
+-- INSERT DATA TO BILL_TO_CI TABLE
+
+INSERT INTO bill_tos_ci
+(
+	bill_to_no,
+	bill_to_first_name,
+	bill_to_last_name,
+	bill_to_street_1,
+	bill_to_street_2,
+	bill_to_city,
+	bill_to_state,
+	bill_to_zip
+)
+VALUES
+(
+	bill_tos_bill_to_no_seq.NEXTVAL,
+	'Yonnys',
+	'Mendez',
+	'4038 Park Ln',
+	'A',
+	'Palm Springs',
+	'FL',
+	33406
+);
+
+INSERT INTO bill_tos_ci
+(
+	bill_to_no,
+	bill_to_first_name,
+	bill_to_last_name,
+	bill_to_street_1,
+	bill_to_street_2,
+	bill_to_city,
+	bill_to_state,
+	bill_to_zip
+)
+VALUES
+(
+	bill_tos_bill_to_no_seq.NEXTVAL,
+	'Jon',
+	'McArthur',
+	'4039 Saturn Blvd',
+	NULL,
+	'Palm Springs',
+	'FL',
+	33406
+);
+
+INSERT INTO bill_tos_ci
+(
+	bill_to_no,
+	bill_to_first_name,
+	bill_to_last_name,
+	bill_to_street_1,
+	bill_to_street_2,
+	bill_to_city,
+	bill_to_state,
+	bill_to_zip
+)
+VALUES
+(
+	bill_tos_bill_to_no_seq.NEXTVAL,
+	'Travis',
+	'Scott',
+	'2301 S Ocean Blvd',
+	'Suite 1',
+	'Boca Raton',
+	'FL',
+	33487
+);
+
+INSERT INTO bill_tos_ci
+(
+	bill_to_no,
+	bill_to_first_name,
+	bill_to_last_name,
+	bill_to_street_1,
+	bill_to_street_2,
+	bill_to_city,
+	bill_to_state,
+	bill_to_zip
+)
+VALUES
+(
+	bill_tos_bill_to_no_seq.NEXTVAL,
+	'Jan',
+	'Mendez',
+	'123 Lane Street',
+	NULL,
+	'Delray Beach',
+	'FL',
+	33481
+);
+
+-- INSERT DATA TO ITEMS_CI TABLE
+
+INSERT INTO items_ci
+(
+	item_no,
+	item_name,
+	item_description,
+	item_unit_price
+)
+VALUES
+(
+	items_item_no_seq.NEXTVAL,
+	'MASTER',
+	'MASTER CYLINDER SIGHT GLASS',
+	3.99
+);
+
+INSERT INTO items_ci
+(
+	item_no,
+	item_name,
+	item_description,
+	item_unit_price
+)
+VALUES
+(
+	items_item_no_seq.NEXTVAL,
+	'CARB KIT',
+	'CARBURETOR KIT',
+	7.9
+);
+
+INSERT INTO items_ci
+(
+	item_no,
+	item_name,
+	item_description,
+	item_unit_price
+)
+VALUES
+(
+	items_item_no_seq.NEXTVAL,
+	'PLUGS',
+	'SPARK PLUGS',
+	4.05
+);
+
+INSERT INTO items_ci
+(
+	item_no,
+	item_name,
+	item_description,
+	item_unit_price
+)
+VALUES
+(
+	items_item_no_seq.NEXTVAL,
+	'BTZ14S',
+	'BTZ14S BATTERY 78-1383',
+	88.95
+);
+
+INSERT INTO items_ci
+(
+	item_no,
+	item_name,
+	item_description,
+	item_unit_price
+)
+VALUES
+(
+	items_item_no_seq.NEXTVAL,
+	23-1006,
+	'10 MM FUEL FILTER',
+	25.15
+);
+
+INSERT INTO items_ci
+(
+	item_no,
+	item_name,
+	item_description,
+	item_unit_price
+)
+VALUES
+(
+	items_item_no_seq.NEXTVAL,
+	'AIR FILTER',
+	'AIR CLEANER 17215-MG8-771',
+	28.53
+);
+
+INSERT INTO items_ci
+(
+	item_no,
+	item_name,
+	item_description,
+	item_unit_price
+)
+VALUES
+(
+	items_item_no_seq.NEXTVAL,
+	'OIL FILTER',
+	'FILTER GASKET',
+	20.1
+);
+
+INSERT INTO items_ci
+(
+	item_no,
+	item_name,
+	item_description,
+	item_unit_price
+)
+VALUES
+(
+	items_item_no_seq.NEXTVAL,
+	'WHEEL',
+	'STEERING WHEEL',
+	149.99
+);
+
+-- INSERT DATA TO OWNERS_CI TABLE
+
+INSERT INTO owners_ci
+(
+	vehicle_vin,
+	bill_to_no
+)
+VALUES
+(
+	'121V5G1H221457H47',
+	8923
+);
+
+INSERT INTO owners_ci
+(
+	vehicle_vin,
+	bill_to_no
+)
+VALUES
+(
+	'4T4BF3EK6BR115086',
+	8924
+);
+
+INSERT INTO owners_ci
+(
+	vehicle_vin,
+	bill_to_no
+)
+VALUES
+(
+	'2GBHG31MXD4127143',
+	8925
+);
+
+INSERT INTO owners_ci
+(
+	vehicle_vin,
+	bill_to_no
+)
+VALUES
+(
+	'1FUBCYCS35HN42268',
+	8926
+);
+
+INSERT INTO owners_ci
+(
+	vehicle_vin,
+	bill_to_no
+)
+VALUES
+(
+	'1C4GJWAG7CL176718',
+	8923
+);
+
+INSERT INTO owners_ci
+(
+	vehicle_vin,
+	bill_to_no
+)
+VALUES
+(
+	'121V5G1H221457H47',
+	8924
+);
+
+INSERT INTO owners_ci
+(
+	vehicle_vin,
+	bill_to_no
+)
+VALUES
+(
+	'4T4BF3EK6BR115086',
+	8925
+);
+
+INSERT INTO owners_ci
+(
+	vehicle_vin,
+	bill_to_no
+)
+VALUES
+(
+	'2GBHG31MXD4127143',
+	8926
+);
+
+INSERT INTO owners_ci
+(
+	vehicle_vin,
+	bill_to_no
+)
+VALUES
+(
+	'1C4GJWAG7CL176718',
+	8926
+);
+
+-- INSERT DATA TO INVOICES_CI TABLE
+
+INSERT INTO invoices_ci
+(
+	invoice_no,
+	invoice_date,
+	invoice_odometer,
+	invoice_tax,
+	invoice_payment_cred,
+	bill_to_no,
+	vehicle_vin
+)
+VALUES
+(
+	invoices_invoice_no_seq.NEXTVAL,
+	date '2017-10-11',
+	88000,
+	5.93,
+	7.00,
+	8923,
+	'121V5G1H221457H47'
+);
+
+INSERT INTO invoices_ci
+(
+	invoice_no,
+	invoice_date,
+	invoice_odometer,
+	invoice_tax,
+	invoice_payment_cred,
+	bill_to_no,
+	vehicle_vin
+)
+VALUES
+(
+	invoices_invoice_no_seq.NEXTVAL,
+	date '2017-11-1',
+	35000,
+	22.00,
+	12.00,
+	8924,
+	'4T4BF3EK6BR115086'
+);
+
+INSERT INTO invoices_ci
+(
+	invoice_no,
+	invoice_date,
+	invoice_odometer,
+	invoice_tax,
+	invoice_payment_cred,
+	bill_to_no,
+	vehicle_vin
+)
+VALUES
+(
+	invoices_invoice_no_seq.NEXTVAL,
+	date '2017-12-3',
+	60000,
+	7.80,
+	2.00,
+	8925,
+	'2GBHG31MXD4127143'
+);
+
+INSERT INTO invoices_ci
+(
+	invoice_no,
+	invoice_date,
+	invoice_odometer,
+	invoice_tax,
+	invoice_payment_cred,
+	bill_to_no,
+	vehicle_vin
+)
+VALUES
+(
+	invoices_invoice_no_seq.NEXTVAL,
+	date '2017-12-22',
+	15000,
+	21.10,
+	NULL,
+	8926,
+	'1FUBCYCS35HN42268'
+);
+
+INSERT INTO invoices_ci
+(
+	invoice_no,
+	invoice_date,
+	invoice_odometer,
+	invoice_tax,
+	invoice_payment_cred,
+	bill_to_no,
+	vehicle_vin
+)
+VALUES
+(
+	invoices_invoice_no_seq.NEXTVAL,
+	date '2017-12-28',
+	17000,
+	22.30,
+	2.00,
+	8923,
+	'1C4GJWAG7CL176718'
+);
+
+INSERT INTO invoices_ci
+(
+	invoice_no,
+	invoice_date,
+	invoice_odometer,
+	invoice_tax,
+	invoice_payment_cred,
+	bill_to_no,
+	vehicle_vin
+)
+VALUES
+(
+	invoices_invoice_no_seq.NEXTVAL,
+	date '2018-1-2',
+	19000,
+	21.10,
+	8.00,
+	8924,
+	'121V5G1H221457H47'
+);
+
+INSERT INTO invoices_ci
+(
+	invoice_no,
+	invoice_date,
+	invoice_odometer,
+	invoice_tax,
+	invoice_payment_cred,
+	bill_to_no,
+	vehicle_vin
+)
+VALUES
+(
+	invoices_invoice_no_seq.NEXTVAL,
+	date '2018-1-5',
+	25000,
+	52.00,
+	12.00,
+	8925,
+	'4T4BF3EK6BR115086'
+);
+
+INSERT INTO invoices_ci
+(
+	invoice_no,
+	invoice_date,
+	invoice_odometer,
+	invoice_tax,
+	invoice_payment_cred,
+	bill_to_no,
+	vehicle_vin
+)
+VALUES
+(
+	invoices_invoice_no_seq.NEXTVAL,
+	date '2018-1-9',
+	26000,
+	60.00,
+	15.50,
+	8926,
+	'2GBHG31MXD4127143'
+);
+
+INSERT INTO invoices_ci
+(
+	invoice_no,
+	invoice_date,
+	invoice_odometer,
+	invoice_tax,
+	invoice_payment_cred,
+	bill_to_no,
+	vehicle_vin
+)
+VALUES
+(
+	invoices_invoice_no_seq.NEXTVAL,
+	date '2018-1-20',
+	88000,
+	60.00,
+	18.50,
+	8926,
+	'1C4GJWAG7CL176718'
+);
+
+INSERT INTO invoices_ci
+(
+	invoice_no,
+	invoice_date,
+	invoice_odometer,
+	invoice_tax,
+	invoice_payment_cred,
+	bill_to_no,
+	vehicle_vin
+)
+VALUES
+(
+	invoices_invoice_no_seq.NEXTVAL,
+	date '2018-2-1',
+	90,
+	52.00,
+	12.20,
+	8925,
+	'4T4BF3EK6BR115086'
+);
+
+INSERT INTO invoices_ci
+(
+	invoice_no,
+	invoice_date,
+	invoice_odometer,
+	invoice_tax,
+	invoice_payment_cred,
+	bill_to_no,
+	vehicle_vin
+)
+VALUES
+(
+	invoices_invoice_no_seq.NEXTVAL,
+	date '2018-2-23',
+	3000,
+	61.50,
+	11.20,
+	8926,
+	'2GBHG31MXD4127143'
+);
+
+INSERT INTO invoices_ci
+(
+	invoice_no,
+	invoice_date,
+	invoice_odometer,
+	invoice_tax,
+	invoice_payment_cred,
+	bill_to_no,
+	vehicle_vin
+)
+VALUES
+(
+	invoices_invoice_no_seq.NEXTVAL,
+	date '2018-3-5',
+	6000,
+	65.00,
+	12.00,
+	8926,
+	'1C4GJWAG7CL176718'
+);
+
+-- INSERT DATA TO ITEMS_SOLD_CI TABLE
+
+INSERT INTO items_sold_ci
+(
+	invoice_no,
+	item_no,
+	item_qty_sold,
+	item_unit_price
+)
+VALUES
+(
+	25333,
+	1122334455,
+	2,
+	3.99
+);
+
+INSERT INTO items_sold_ci
+(
+	invoice_no,
+	item_no,
+	item_qty_sold,
+	item_unit_price
+)
+VALUES
+(
+	25334,
+	1122334456,
+	1,
+	7.9
+);
+
+INSERT INTO items_sold_ci
+(
+	invoice_no,
+	item_no,
+	item_qty_sold,
+	item_unit_price
+)
+VALUES
+(
+	25335,
+	1122334457,
+	2,
+	4.05
+);
+
+INSERT INTO items_sold_ci
+(
+	invoice_no,
+	item_no,
+	item_qty_sold,
+	item_unit_price
+)
+VALUES
+(
+	25336,
+	1122334458,
+	1,
+	88.95
+);
+
+INSERT INTO items_sold_ci
+(
+	invoice_no,
+	item_no,
+	item_qty_sold,
+	item_unit_price
+)
+VALUES
+(
+	25337,
+	1122334459,
+	1,
+	25.15
+);
+
+INSERT INTO items_sold_ci
+(
+	invoice_no,
+	item_no,
+	item_qty_sold,
+	item_unit_price
+)
+VALUES
+(
+	25338,
+	1122334460,
+	1,
+	28.53
+);
+
+INSERT INTO items_sold_ci
+(
+	invoice_no,
+	item_no,
+	item_qty_sold,
+	item_unit_price
+)
+VALUES
+(
+	25339,
+	1122334455,
+	1,
+	3.99
+);
+
+INSERT INTO items_sold_ci
+(
+	invoice_no,
+	item_no,
+	item_qty_sold,
+	item_unit_price
+)
+VALUES
+(
+	25340,
+	1122334456,
+	2,
+	7.9
+);
+
+INSERT INTO items_sold_ci
+(
+	invoice_no,
+	item_no,
+	item_qty_sold,
+	item_unit_price
+)
+VALUES
+(
+	25341,
+	1122334457,
+	2,
+	4.05
+);
+
+INSERT INTO items_sold_ci
+(
+	invoice_no,
+	item_no,
+	item_qty_sold,
+	item_unit_price
+)
+VALUES
+(
+	25342,
+	1122334458,
+	1,
+	88.95
+);
+
+INSERT INTO items_sold_ci
+(
+	invoice_no,
+	item_no,
+	item_qty_sold,
+	item_unit_price
+)
+VALUES
+(
+	25343,
+	1122334459,
+	1,
+	25.15
+);
+
+INSERT INTO items_sold_ci
+(
+	invoice_no,
+	item_no,
+	item_qty_sold,
+	item_unit_price
+)
+VALUES
+(
+	25344,
+	1122334460,
+	1,
+	28.53
+);
+
+INSERT INTO items_sold_ci
+(
+	invoice_no,
+	item_no,
+	item_qty_sold,
+	item_unit_price
+)
+VALUES
+(
+	25333,
+	1122334460,
+	1,
+	28.53
+);
+
+INSERT INTO items_sold_ci
+(
+	invoice_no,
+	item_no,
+	item_qty_sold,
+	item_unit_price
+)
+VALUES
+(
+	25334,
+	1122334459,
+	1,
+	25.15
+);
+
+INSERT INTO items_sold_ci
+(
+	invoice_no,
+	item_no,
+	item_qty_sold,
+	item_unit_price
+)
+VALUES
+(
+	25335,
+	1122334458,
+	4,
+	88.95
+);
+
+INSERT INTO items_sold_ci
+(
+	invoice_no,
+	item_no,
+	item_qty_sold,
+	item_unit_price
+)
+VALUES
+(
+	25336,
+	1122334457,
+	2,
+	4.05
+);
+
+INSERT INTO items_sold_ci
+(
+	invoice_no,
+	item_no,
+	item_qty_sold,
+	item_unit_price
+)
+VALUES
+(
+	25337,
+	1122334456,
+	1,
+	7.9
+);
+
+INSERT INTO items_sold_ci
+(
+	invoice_no,
+	item_no,
+	item_qty_sold,
+	item_unit_price
+)
+VALUES
+(
+	25338,
+	1122334455,
+	3,
+	3.99
+);
+
+INSERT INTO items_sold_ci
+(
+	invoice_no,
+	item_no,
+	item_qty_sold,
+	item_unit_price
+)
+VALUES
+(
+	25339,
+	1122334460,
+	1,
+	28.53
+);
+
+INSERT INTO items_sold_ci
+(
+	invoice_no,
+	item_no,
+	item_qty_sold,
+	item_unit_price
+)
+VALUES
+(
+	25340,
+	1122334459,
+	1,
+	25.15
+);
+
+INSERT INTO items_sold_ci
+(
+	invoice_no,
+	item_no,
+	item_qty_sold,
+	item_unit_price
+)
+VALUES
+(
+	25341,
+	1122334458,
+	2,
+	88.95
+);
+
+INSERT INTO items_sold_ci
+(
+	invoice_no,
+	item_no,
+	item_qty_sold,
+	item_unit_price
+)
+VALUES
+(
+	25342,
+	1122334457,
+	2,
+	4.05
+);
+
+INSERT INTO items_sold_ci
+(
+	invoice_no,
+	item_no,
+	item_qty_sold,
+	item_unit_price
+)
+VALUES
+(
+	25343,
+	1122334456,
+	2,
+	7.9
+);
+
+INSERT INTO items_sold_ci
+(
+	invoice_no,
+	item_no,
+	item_qty_sold,
+	item_unit_price
+)
+VALUES
+(
+	25344,
+	1122334455,
+	2,
+	3.99
+);
+
+INSERT INTO items_sold_ci
+(
+	invoice_no,
+	item_no,
+	item_qty_sold,
+	item_unit_price
+)
+VALUES
+(
+	25333,
+	1122334458,
+	1,
+	88.95
+);
+
+-- END INSERT STATEMENTS
---
+++ Mod 3 Deliverables/FuentesRefsniderFile3.sql
@@ -0,0 +1,311 @@
+-- JAY FUENTES AND MATT REFSNIDER
+
+-- START SELECT STATEMENTS
+
+-- SET LINESIZE AND PAGESIZE
+
+SET LINESIZE 200
+SET PAGESIZE 66
+
+
+
+-- SELECT ALL COLUMNS FROM VEHICLES_CI
+
+COLUMN "VIN" FORMAT A17;
+COLUMN "MAKE" FORMAT A8;
+COLUMN "MODEL" FORMAT A11;
+COLUMN "YEAR" FORMAT 9999;
+COLUMN "COLOR" FORMAT A10;
+COLUMN "DEALER" FORMAT A6;
+
+SELECT vehicle_vin "VIN", vehicle_make "MAKE",
+		vehicle_model "MODEL", vehicle_year "YEAR",
+		vehicle_color "COLOR", vehicle_dealer_no "DEALER"
+	FROM vehicles_ci;
+
+-- SELECT ALL COLUMNS FROM BILL_TOS_CI
+
+COLUMN "BILL TO #" FORMAT 9999;
+COLUMN "FIRST NAME" FORMAT A10;
+COLUMN "LAST NAME" FORMAT A9;
+COLUMN "STREET" FORMAT A17;
+COLUMN "APT" FORMAT A7;
+COLUMN "CITY" FORMAT A12;
+COLUMN "STATE" FORMAT A5;
+COLUMN "ZIP" FORMAT A5;
+
+SELECT bill_to_no "BILL TO #", bill_to_first_name "FIRST NAME",
+		bill_to_last_name "LAST NAME", bill_to_street_1 "STREET",
+		bill_to_street_2 "APT", bill_to_city "CITY",
+		bill_to_state "STATE", bill_to_zip "ZIP"
+	FROM bill_tos_ci;
+
+-- SELECT ALL COLUMNS FROM ITEMS_CI
+
+COLUMN "ITEM #" FORMAT 9999999999;
+COLUMN "ITEM NAME" FORMAT A10;
+COLUMN "DESCRIPTION" FORMAT A27;
+COLUMN "UNIT PRICE" FORMAT $999.99;
+
+SELECT item_no "ITEM #", item_name "ITEM NAME",
+		item_description "DESCRIPTION", item_unit_price "UNIT PRICE"
+	FROM items_ci;
+
+-- SELECT ALL COLUMNS FROM OWNERS_CI
+
+COLUMN "VIN" FORMAT A17;
+COLUMN "BILL TO #" FORMAT 9999;
+
+SELECT vehicle_vin "VIN", bill_to_no "BILL TO #"
+	FROM owners_ci;
+
+-- SELECT ALL COLUMNS FROM INVOICES_CI
+
+COLUMN "INVOICE #" FORMAT 99999999;
+COLUMN "DATE Y/M/D" FORMAT A10;
+COLUMN "ODOMETER" FORMAT 9999999;
+COLUMN "TAX" FORMAT $99.99;
+COLUMN "PAYMENT" FORMAT $999.99;
+COLUMN "BILL TO #" FORMAT 9999;
+COLUMN "VIN" FORMAT A17;
+
+SELECT invoice_no "INVOICE #", TO_CHAR(invoice_date, 'YY/MM/DD') "DATE Y/M/D",
+		invoice_odometer "ODOMETER", invoice_tax "TAX",
+		invoice_payment_cred "PAYMENT", bill_to_no "BILL TO #",
+		vehicle_vin "VIN"
+	FROM invoices_ci;
+
+-- SELECT ALL COLUMNS FROM ITEMS_SOLD_CI
+
+COLUMN "INVOICE #" FORMAT 99999999;
+COLUMN "ITEM #" FORMAT 999999999999999;
+COLUMN "ITEM QTY SOLD" FORMAT 9999;
+COLUMN "UNIT PRICE" FORMAT $999,999.99;
+
+SELECT invoice_no "INVOICE #", item_no "ITEM #",
+		item_qty_sold "ITEM QTY SOLD", item_unit_price "UNIT PRICE"
+	FROM items_sold_ci;
+
+-- SORT INVOICES_CI BY INVOICE_DATE IN ASCENDING ORDER
+
+COLUMN "INVOICE #" FORMAT 99999999;
+COLUMN "DATE Y/M/D" FORMAT A10;
+COLUMN "ODOMETER" FORMAT 9999999;
+COLUMN "TAX" FORMAT $99.99;
+COLUMN "PAYMENT" FORMAT $999.99;
+COLUMN "BILL TO #" FORMAT 9999;
+COLUMN "VIN" FORMAT A17;
+
+SELECT invoice_no "INVOICE #", TO_CHAR(invoice_date, 'YY/MM/DD') "DATE Y/M/D",
+		invoice_odometer "ODOMETER", invoice_tax "TAX",
+		invoice_payment_cred "PAYMENT", bill_to_no "BILL TO #",
+		vehicle_vin "VIN"
+	FROM invoices_ci
+	ORDER BY "DATE Y/M/D" ASC;
+
+-- SORT ITEMS_SOLD_CI BY ITEM_QTY_SOLD IN DESCENDING ORDER
+
+COLUMN "INVOICE #" FORMAT 99999999;
+COLUMN "ITEM #" FORMAT 999999999999999;
+COLUMN "ITEM QTY SOLD" FORMAT 9999;
+COLUMN "UNIT PRICE" FORMAT $999,999.99;
+
+SELECT invoice_no "INVOICE #", item_no "ITEM #",
+		item_qty_sold "ITEM QTY SOLD", item_unit_price "UNIT PRICE"
+	FROM items_sold_ci
+	ORDER BY "ITEM QTY SOLD" DESC;
+
+-- SORT BILL_TOS_CI BY CITY THEN LAST_NAME IN ASCENDING ORDER
+
+COLUMN "BILL TO #" FORMAT 9999;
+COLUMN "FIRST NAME" FORMAT A10;
+COLUMN "LAST NAME" FORMAT A15;
+COLUMN "STREET" FORMAT A20;
+COLUMN "APT" FORMAT A13;
+COLUMN "CITY" FORMAT A15;
+COLUMN "STATE" FORMAT A6;
+COLUMN "ZIP" FORMAT A5;
+
+SELECT bill_to_no "BILL TO #", bill_to_first_name "FIRST NAME",
+		bill_to_last_name "LAST NAME", bill_to_street_1 "STREET",
+		bill_to_street_2 "APT", bill_to_city "CITY",
+		bill_to_state "STATE", bill_to_zip "ZIP"
+	FROM bill_tos_ci
+	ORDER BY "CITY", "LAST NAME" ASC;
+
+-- SORT VEHICLES BY VEHICLE_YEAR THEN VEHICLE_ODOMETER IN ASCENDING ORDER
+
+COLUMN "INVOICE #" FORMAT 99999999;
+COLUMN "ITEM #" FORMAT 999999999999999;
+COLUMN "ITEM QTY SOLD" FORMAT 9999;
+COLUMN "UNIT PRICE" FORMAT $999,999.99;
+
+SELECT UNIQUE invoice_no "INVOICE #", item_no "ITEM #",
+		item_qty_sold "ITEM QTY SOLD", item_unit_price "UNIT PRICE"
+	FROM items_sold_ci
+	ORDER BY "INVOICE #", "UNIT PRICE" DESC;
+
+-- SELECT ALL ITEMS WITH "FILTER" IN NAME
+
+COLUMN "ITEM #" FORMAT 999999999999999;
+COLUMN "ITEM NAME" FORMAT A13;
+COLUMN "DESCRIPTION" FORMAT A30;
+COLUMN "UNIT PRICE" FORMAT $999,999.99;
+
+SELECT item_no "ITEM #", item_name "ITEM NAME",
+		item_description "DESCRIPTION", item_unit_price "UNIT PRICE"
+	FROM items_ci
+	WHERE item_name LIKE '%FILTER%'
+	ORDER BY "ITEM #";
+
+-- SELECT ITEMS SOLD 2 TIMES OR LESS GREATER THAN $20
+
+COLUMN "INVOICE #" FORMAT 99999999;
+COLUMN "ITEM #" FORMAT 999999999999999;
+COLUMN "ITEM QTY SOLD" FORMAT 9999;
+COLUMN "UNIT PRICE" FORMAT $999,999.99;
+
+SELECT invoice_no "INVOICE #", item_no "ITEM #",
+		item_qty_sold "ITEM QTY SOLD", item_unit_price "UNIT PRICE"
+	FROM items_sold_ci
+	WHERE item_qty_sold <= 2
+	AND item_unit_price >= 20
+	ORDER BY "UNIT PRICE" DESC, "ITEM QTY SOLD" DESC;
+
+-- SELECT BILL TO NO 8923
+
+COLUMN "VIN" FORMAT A17;
+COLUMN "BILL TO #" FORMAT 9999;
+
+SELECT vehicle_vin "VIN", bill_to_no "BILL TO #"
+	FROM owners_ci
+	WHERE bill_to_no = '8923'
+	ORDER BY "VIN";
+
+-- SELECT ALL ITEMS IN $25-$199 RANGE
+
+COLUMN "ITEM #" FORMAT 999999999999999;
+COLUMN "ITEM NAME" FORMAT A13;
+COLUMN "DESCRIPTION" FORMAT A30;
+COLUMN "UNIT PRICE" FORMAT $999,999.99;
+
+SELECT item_no "ITEM #", item_name "ITEM NAME",
+		item_description "DESCRIPTION", item_unit_price "UNIT PRICE"
+	FROM items_ci
+	WHERE item_unit_price >= 25
+	AND item_unit_price <= 199
+	ORDER BY "ITEM #";
+
+-- INNER JOIN OF BILL_TOS_CI AND OWNERS_CI
+
+COLUMN "BILL TO #" FORMAT 9999;
+COLUMN "OWNER NAME" FORMAT A20;
+OLUMN "VIN" FORMAT A18;
+
+SELECT bill_tos_ci.bill_to_no "BILL TO #",
+		bill_tos_ci.bill_to_first_name || ' ' || bill_tos_ci.bill_to_last_name "OWNER NAME",
+		owners_ci.vehicle_vin "VIN"
+	FROM bill_tos_ci
+		INNER JOIN owners_ci
+		ON bill_tos_ci.bill_to_no = owners_ci.bill_to_no
+		ORDER BY "BILL TO #";
+
+-- OUTER JOIN OF ITEMS_CI AND ITEMS_SOLD_CI
+
+COLUMN "ITEM #" FORMAT 999999999999999;
+COLUMN "ITEM NAME" FORMAT A13;
+COLUMN "DESCRIPTION" FORMAT A30;
+COLUMN "UNIT PRICE" FORMAT $999,999.99;
+COLUMN "INVOICE #" FORMAT 99999999;
+
+SELECT items_ci.item_no "ITEM #", items_ci.item_name "ITEM NAME",
+		items_ci.item_description "DESCRIPTION", items_ci.item_unit_price "UNIT PRICE",
+		items_sold_ci.invoice_no "INVOICE #"
+	FROM items_ci LEFT OUTER JOIN items_sold_ci
+	ON items_ci.item_unit_price = 7.9
+	ORDER BY "ITEM #", "INVOICE #";
+
+-- INNER JOIN OF BILL_TOS_CI, VEHICLES_CI, INVOICES_CI, AND ITEMS_SOLD_CI
+
+COLUMN "OWNER NAME" FORMAT A15;
+COLUMN "INVOICE #" FORMAT 99999999;
+COLUMN "DATE Y/M/D" FORMAT A10;
+COLUMN "ITEM #" FORMAT 999999999999999;
+COLUMN "UNIT PRICE" FORMAT $999,999.99;
+COLUMN "ITEM QTY SOLD" FORMAT 9999;
+COLUMN "YEAR" FORMAT 9999;
+COLUMN "MAKE" FORMAT A8;
+COLUMN "MODEL" FORMAT A11;
+COLUMN "VIN" FORMAT A17;
+COLUMN "ODOMETER" FORMAT 99999999;
+
+SELECT bill_tos_ci.bill_to_first_name || ' ' || bill_tos_ci.bill_to_last_name "OWNER NAME",
+		invoices_ci.invoice_no "INVOICE #", TO_CHAR(invoice_date, 'YY/MM/DD') "DATE Y/M/D",
+		items_sold_ci.item_no "ITEM #", items_sold_ci.item_unit_price "UNIT PRICE",
+		items_sold_ci.item_qty_sold "ITEM QTY SOLD", vehicles_ci.vehicle_year "YEAR",
+		vehicles_ci.vehicle_make "MAKE", vehicles_ci.vehicle_model "MODEL"
+	FROM invoices_ci
+	INNER JOIN vehicles_ci
+		ON invoices_ci.vehicle_vin = vehicles_ci.vehicle_vin
+	INNER JOIN bill_tos_ci
+		ON invoices_ci.bill_to_no = bill_tos_ci.bill_to_no
+	INNER JOIN items_sold_ci
+		ON invoices_ci.invoice_no = items_sold_ci.invoice_no
+	ORDER BY "OWNER NAME", "INVOICE #" DESC, "ITEM #" DESC;
+
+-- SUBTRACT SOLD ITEMS FROM TOTAL ITEMS
+
+COLUMN "UNSOLD ITEMS" FORMAT A12;
+
+SELECT item_no "UNSOLD ITEMS"
+	FROM items_ci
+	MINUS
+	SELECT item_no
+	FROM items_sold_ci;
+
+-- CALCULATE INVOICE TOTALS
+
+COLUMN "INVOICE #" FORMAT 99999999;
+COLUMN "INVOICE TOTAL" FORMAT $9999.99;
+
+SELECT invoice_no "INVOICE #",
+		SUM((((item_qty_sold * item_unit_price) + invoice_tax) - NVL(invoice_payment_cred, 0))) "INVOICE TOTAL"
+	FROM items_sold_ci JOIN invoices_ci USING (invoice_no)
+	GROUP BY (invoice_no)
+	ORDER BY invoice_no;
+
+-- COUNT INVOICES
+
+COLUMN "TOTAL INVOICES" FORMAT 99999
+
+SELECT COUNT(*) "TOTAL INVOICES"
+	FROM invoices_ci;
+
+-- COUNT ITEMS
+
+COLUMN "TOTAL ITEMS" FORMAT 99999
+
+SELECT COUNT(*) "TOTAL ITEMS"
+	FROM items_ci;
+
+-- CALCULATE MAX ITEM_UNIT_PRICE
+
+COLUMN "ITEM #" FORMAT 9999999999;
+COLUMN "ITEM NAME" FORMAT A10;
+COLUMN "DESCRIPTION" FORMAT A27;
+COLUMN "UNIT PRICE" FORMAT $999,999.99;
+
+SELECT MAX(item_unit_price) "MAX UNIT PRICE"
+	FROM items_ci;
+
+-- CALCULATE MIN ITEM_UNIT_PRICE
+
+COLUMN "ITEM #" FORMAT 9999999999;
+COLUMN "ITEM NAME" FORMAT A10;
+COLUMN "DESCRIPTION" FORMAT A27;
+COLUMN "UNIT PRICE" FORMAT $999,999.99;
+
+SELECT MIN(item_unit_price) "MIN UNIT PRICE"
+	FROM items_ci;
+
+-- END SELECT STATEMENTS
